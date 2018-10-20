502839415db5e632998cb1ab966c41f3bf603385
Titil364 <ravetoute@gmail.com>
2018-10-20 20:31:06 +0200

    Read the valuation of the variable in a file and put it in vectors

--- Cpp/src/dotMaker.cpp
+++ Cpp/src/dotMaker.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <unistd.h>
+#include <vector>
 
 const std::string DATA_FOLDER = "data/";
 const std::string FILE_EXTENSION = ".dot";
@@ -30,6 +31,18 @@ void export_file_as_png(std::string in)
 }
 
 /**
+	\brief Display on the screen the value of the vector (must be a string vector)
+	\param v A String vector
+ */
+void display_string_vector(std::vector< std::string> v){
+	for(std::vector< std::string>::iterator it = v.begin(); it!=v.end(); ++it){
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+}
+
+
+/**
 	\brief  
 	\param SAT_ANSWER The name of the file used to know if the formula is satisfiable
  */
@@ -46,6 +59,31 @@ int construct_triangle_partition(std::string SAT_ANSWER, std::string out){
 			std::cout << "The formula is UNSAT. Please choose an other one. " << std::endl;
 			return 2;
 		}
+		
+		size_t i = 0;
+		std::vector< std::string> Tvariables;
+		std::vector< std::string> Fvariables;
+		std::vector< std::string> variables;
+		std::string delimiter(" "), token;
+		while((i = line.find(delimiter)) != std::string::npos){
+			token = line.substr(0, i);
+			//Token displayer
+			//std::cout << token << std::endl;
+			if(token.at(0) == '-'){
+				Fvariables.push_back(token.substr(1, token.length()));
+			}else{
+				Tvariables.push_back(token);
+			}
+			variables.push_back(token);
+			line.erase(0, i + delimiter.length());
+		}
+		std::cout << "Positive variables : " << std::endl;
+		display_string_vector(Tvariables);	
+		std::cout << "Negative variables : " << std::endl;
+		display_string_vector(Fvariables);	
+		std::cout << "All variables : " << std::endl;
+		display_string_vector(variables);	
+		//Il faut détecter qui est le voisin de qui
 		std::cout << line <<std::endl;
 		
 	}else{
